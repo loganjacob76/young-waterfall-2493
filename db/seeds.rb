c68e7d8f83e060931ea33d840942b3e310bc3548
loganjacob76 <nagolisback@gmail.com>
2021-04-12 09:52:38 -0600

    Created seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Movie.destroy_all
+Studio.destroy_all
+Actor.destroy_all
+MovieActor.destroy_all
+
+studio1 = Studio.create!(name: 'Universal Studios', location: 'Hollywood')
+studio2 = Studio.create!(name: 'Lucasfilm Ltd.', location: 'San Francisco')
+
+movie1 = studio1.movies.create!(title: 'Raiders of the Lost Ark', creation_year: 1981, genre: 'Action/Adventure')
+movie2 = studio2.movies.create!(title: 'A New Hope', creation_year: 1977, genre: 'Sci-fi')
+
+actor1 = Actor.create!(name: 'Harrison Ford', age: 78, currently_working: false)
+
+MovieActor.create!(movie: movie1, actor: actor1)
+MovieActor.create!(movie: movie2, actor: actor1)
